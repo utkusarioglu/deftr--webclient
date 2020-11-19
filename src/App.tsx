10c1d0ec265a7a6ff87a28c69919c0f84a424001
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-11-19 22:13:43 +0300

    Feature: add very basic server fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ServerResponse } from '../../api/src';
 
 function App() {
+  const request = Math.random().toString().replace('.', '').slice(1, 6);
+  const initial: ServerResponse = {
+    test: request,
+    response: 'waiting',
+  };
+  const [response, setResponse] = useState<ServerResponse>(initial);
+
+  useEffect(() => {
+    const location = window.location.href.slice(0, -1);
+    fetch(`${location}:4000/${request}`)
+      .then((data) => {
+        return data.text();
+      })
+      .then((body) => {
+        const bodyJson: ServerResponse = JSON.parse(body);
+        setResponse(bodyJson);
+      });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,8 @@ function App() {
         >
           Learn React
         </a>
+        <span>{response.test}</span>
+        <span>{response.response}</span>
       </header>
     </div>
   );
