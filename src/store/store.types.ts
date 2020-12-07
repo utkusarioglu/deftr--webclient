dd2abacee93f26ef04d5ba4bb564774bb2baaac8
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-12-07 14:28:38 +0300

    Chore: merge action types with store types
    - Types related to actions were kept as a separate file. Upon
    examination, combining these two modules seemed more descriptive
    and easier to grasp.

--- src/store/store.types.ts
+++ src/store/store.types.ts
@@ -1,8 +1,11 @@
+import type { ServerConnection } from '../services/server-connection/server-connection.types';
+import { ACTION_TYPES, ACTION_STATES } from './store.constants';
+
 /**
  * Collects all the types from all the sources that contribute to the state.
  */
-export default interface IRootReducer {
-  app: any;
+export default interface RootReducer {
+  serverConnection: ServerConnection;
 }
 
 /*
@@ -13,3 +16,44 @@ This project treats enums as constants and adops the naming convention
 for constants for enums.
 */
 export class ERROR_CODES {}
+
+/**
+ * Flux standard action SUCCESS
+ *
+ * @privateRemarks
+ * type property needs more work to reach a good type casting solution. The
+ * intent is to have a type that combines all the type enums from all
+ * the services and features that contribute to the state.
+ */
+interface FSASuccess<T> {
+  state: ACTION_STATES.SUCCESS;
+  type: ACTION_TYPES;
+  payload: T;
+}
+
+/**
+ * Flux standard action fail
+ *
+ * @privateRemarks
+ * type property needs more work to reach a good type casting solution. The
+ * intent is to have a type that combines all the type enums from all
+ * the services and features that contribute to the state.
+ */
+interface FSAFailure {
+  state: ACTION_STATES.FAIL;
+  type: ACTION_TYPES;
+  errorCode: ERROR_CODES;
+  meta?: any;
+}
+
+/**
+ * Aggregate type for Flux standard actions
+ * Both success and failure states, the distinction is made by the
+ * "state" property
+ */
+export type FSA<T> = FSASuccess<T> | FSAFailure;
+
+/**
+ * Dispatch method for redux
+ */
+export type DispatchMethod<T> = (vars: FSA<T>) => void;
